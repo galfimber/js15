94c93943c6d44f4c40d90b100e9f6efc04571bff
Nikita Varankin <galfimber2014@yandex.ru>
2023-12-07 21:25:47 +0500

    Выполнено усложненное задание 15

--- script.js
+++ script.js
@@ -1,39 +1,51 @@
-const DomElement = function (selector, height, width, bg, fontSize) {
-  this.selector = selector;
+"use strict";
+
+const DomElement = function (height, width, bg) {
   this.height = height;
   this.width = width;
   this.bg = bg;
-  this.fontSize = fontSize;
   this.newElement = function () {
-    switch (true) {
-      case this.selector[0] === ".":
-        const divv = document.createElement("div");
-        divv.classList.add(this.selector.slice(1));
-        divv.style.height = this.height + "px";
-        divv.style.width = this.width + "px";
-        divv.style.background = this.bg;
-        divv.style.fontSize = this.fontSize + "px";
-        divv.innerHTML = "Hello its tag div";
-        document.body.append(divv);
-        console.log("div");
-        break;
-      case this.selector[0] === "#":
-        const pp = document.createElement("p");
-        pp.id = this.selector.slice(1);
-        pp.style.height = this.height + "px";
-        pp.style.width = this.width + "px";
-        pp.style.background = this.bg;
-        pp.style.fontSize = this.fontSize + "px";
-        pp.innerHTML = "Hello its tag p";
-        document.body.append(pp);
-        console.log("p");
-        break;
-    }
+    const div = document.createElement("div");
+    div.style.height = this.height + "px";
+    div.style.width = this.width + "px";
+    div.style.background = this.bg;
+    div.style.position = "absolute";
+    div.style.left = "0";
+    div.style.top = "0";
+    document.body.append(div);
   };
 };
+let leftRight = 0;
+let upDown = 0;
 
-const newDiv = new DomElement(".header", "100", "200", "blue", "20");
-const newP = new DomElement("#text", "50", "50", "red", "12");
+const square = new DomElement("100", "100", "blue");
 
-newDiv.newElement();
-newP.newElement();
+document.addEventListener("DOMContentLoaded", square.newElement());
+
+document.addEventListener("keydown", function (e) {
+  const squareEl = document.querySelector("div");
+  const arrow = {
+    left: "ArrowLeft",
+    up: "ArrowUp",
+    right: "ArrowRight",
+    down: "ArrowDown",
+  };
+  switch (e.key) {
+    case arrow.left:
+      leftRight -= 10;
+      squareEl.style.left = leftRight + "px";
+      break;
+    case arrow.up:
+      upDown -= 10;
+      squareEl.style.top = upDown + "px";
+      break;
+    case arrow.right:
+      leftRight += 10;
+      squareEl.style.left = leftRight + "px";
+      break;
+    case arrow.down:
+      upDown += 10;
+      squareEl.style.top = upDown + "px";
+      break;
+  }
+});
